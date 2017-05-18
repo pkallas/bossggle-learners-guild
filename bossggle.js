2d4f6c9601c9fc1be8d267a652338ecf265b5e48
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-05-17 17:40:55 -0700

    Made changes to js file to work with buttons

--- bossggle.js
+++ bossggle.js
@@ -1,3 +1,9 @@
+// Use onload command to have page load with random words and correct colors
+window.onload = function () {
+  randomLetter()
+  vowelCheck()
+};
+
 /* Create a function to obtain a value between 65 and 90, the Unicode values for
 the letters between capital A and capital Z */
 function getIntForLetter(min, max) {
@@ -14,85 +20,101 @@ function randomLetter() {
     var randomNumber = getIntForLetter();
     boxLetter[i].innerHTML = String.fromCodePoint(randomNumber);
   };
-// Include a statement to guarentee that at least one vowel is drawn
-  if (boxLetter.indexOf(65) === -1 || boxLetter.indexOf(69) === -1 ||
-      boxLetter.indexOf(73) === -1 || boxLetter.indexOf(79) === -1 ||
-      boxLetter.indexOf(85) === -1) {
-        for (var i = 0; i < boxLetter.length; i++) {
-          var randomNumber = getIntForLetter();
-          boxLetter[i].innerHTML = String.fromCodePoint(randomNumber);
-    };
+};
+
+// Include a function to guarentee that at least one vowel is drawn
+function vowelCheck() {
+  var boxLetter = document.getElementsByClassName("inner");
+  for (var i = 0; i < boxLetter.length; i++) {
+    boxLetter[i].innerHTML
+  };
+  if (boxLetter.innerHTML.indexOf('A') === -1 || boxLetter.innerHTML.indexOf('E') === -1 ||
+      boxLetter.innerHTML.indexOf('I') === -1 || boxLetter.innerHTML.indexOf('O') === -1 ||
+      boxLetter.innerHTML.indexOf('I') === -1)  {
+        randomLetter();
   };
 };
 
-/* Create a function to be used to add clicked letter to displayedWord element,
+/* Create a function to be used to add clicked letter to words element,
 and change the color of the box to orange */
 function buttonClick() {
-  var letter = document.getElementsByClassName("inner");
-  letter.style.color = 'rgb(255, 165, 0)';
-  var display = document.getElementById("displayedWord");
-  document.getElementById("displayedWord").innerHTML = display.push(letter.innerHTML);
+  var letter = event.target.innerHTML;
+  var display = document.getElementsByClassName("words").innerHTML;
+  display.push(letter);
+  display.join('');
+  event.target.style.backgroundColor = '#FEA221';
+  event.target.disabled = true;
 };
 /* Add an event handler to utilize the buttonClick function; this function will
 remain commented out until it proves necessary */
-// document.getElementsByClassName("inner").addEventListener("click", buttonClick());
+// var innerClass = document.getElementsByClassName("inner");
+// for (var i = 0; i < innerClass.length; i++) {
+//   innerClass[i].addEventListener("click", buttonClick());
+// };
 
-/* Create a function that takes the length of a string and multiplies it by 9,
-then adds that value to points to use as a submit button */
-function submitWord() {
-  var display = document.getElmentById("displayedWord");
-  document.getElementById("points").innerHTML =
-    document.getElementById("points").unshift(display.length * 9);
-  document.getElementById("Score").innerHTML =
-    document.getElementById("Score").innerHTML + display.length * 9;
-  document.getElementById("list").unshift(displayedWord);
-};
-
-// Create a function that clears the displayedWord and all selected letters
+// Create a function that clears the words and all selected letters
 function clearWord() {
 // Loop through all of the boxes
  var boxLetter = document.getElementsByClassName('inner');
     for (var i = 0; i < boxLetter.length; i++) {
 //  If the color of the current box is orange, change it to solid gray
-     if (boxLetter[i].style.color === 'rgb(255, 165, 0)')
-      { boxLetter[i].style.color = 'rgb(128, 128, 128)';
+     if (boxLetter[i].style.backgroundColor === '#FEA221')
+      { boxLetter[i].style.backgroundColor = '#999999';
+        boxLetter[i].disabled = false;
      };
    };
-   return document.getElementById("displayedWord").innerHTML = [];
-// };
+  document.getElementsByClassName("words").innerHTML = [];
+};
+
+/* Create a function that takes the length of a string and multiplies it by 9,
+then adds that value to points to use as a submit button */
+function submitWord() {
+  var displayedWord = document.getElementsByClassName("words").innerHTML;
+  var pointsDisplay = document.getElementsByClassName("points").innerHTML;
+  var wordsDisplay = document.getElementsByClassName("list").innerHTML;
+  var playerScore = document.getElementsByClassName("number").innerHTML;
+  var pointsValue = displayedWord.length * 9;
+  playerScore = playerScore + pointsValue;
+  pointsDisplay.unshift(pointsValue);
+  displayedWord.split();
+  wordsDisplay.unshift(displayedWord);
+  wordsDisplay.split('');
+  clearword();
+};
+
 
 /* Create a function to reset the board, clearing the current word and
 re-randomizing the letters in the table */
 function resetWord() {
   clearWord();
+  document.getElementsByClassName("points").innerHTML = [];
+  document.getElementsByClassName("list").innerHTML = [];
+  document.getElementsByClassName("number").innerHTML = [];
   randomLetter();
+  vowelCheck();
 };
 
-Create a function to unfade the submit button
+// Create a function to unfade the submit button
 function unfadeSubmit() {
   var submitButton = document.getElementsByClassName("submit");
-  submitButton.style.color = #C7E69A
+  submitButton.style.backgroundColor = '#C7E69A'
 };
-
+//
 // Create a function to unfade clear button
 function unfadeClear() {
   var clearButton = document.getElementsByClassName("clear");
-  clearButton.style.color = #F3AEB5
-}
+  clearButton.style.backgroundColor = '#F3AEB5'
+};
 // Add event listener for submit button to change color
-document.getElementsByClassName("submit").addEventListener("DOMContentLoaded"
-  function() {
-    setTimeout(unfadeSubmit(), 5000);
-});
+var submitClass = document.getElementsByClassName("submit");
+ for (var i = 0; i < submitClass.length; i++) {
+  submitClass[i].addEventListener("DOMContentLoaded", setTimeout(
+    unfadeSubmit(), 5000));
+};
 
 // Add event listener for clear button to change color
-document.getElementsByClassName("clear").addEventListener("DOMContentLoaded"
-  function() {
-    setTimeout(unfadeClear(), 5000);
-});
-
-// Use onload command to have page load with random words and correct colors
-window.onload = function resetWord() {
-  clearWord();
-  randomLetter();
+var clearClass = document.getElementsByClassName("clear");
+ for (var i = 0; i < clearClass.length; i++) {
+  clearClass[i].addEventListener("DOMContentLoaded", setTimeout(
+    unfadeClear(), 5000));
 };
