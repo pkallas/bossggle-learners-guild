4bb4f0d2edef32c010708d3331fdbad5d9511735
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-05-19 10:05:35 -0700

    Changes to js file to fix buttons

--- bossggle.js
+++ bossggle.js
@@ -1,7 +1,6 @@
 // Use onload command to have page load with random words and correct colors
 window.onload = function () {
   randomLetter()
-  vowelCheck()
 };
 
 /* Create a function to obtain a value between 65 and 90, the Unicode values for
@@ -20,85 +19,64 @@ function randomLetter() {
     var randomNumber = getIntForLetter();
     boxLetter[i].innerHTML = String.fromCodePoint(randomNumber);
   };
+  if (!hasVowel()) {
+    randomLetter();
+  }
 };
 
 // Include a function to guarentee that at least one vowel is drawn
-function vowelCheck() {
-  var boxLetter = document.getElementsByClassName("inner");
-  for (var i = 0; i < boxLetter.length; i++) {
-    boxLetter[i].innerHTML
-  };
-  do {
-    randomLetter();
-  }
-  while (boxLetter.innerHTML.indexOf('A', 'E', 'I', 'O', 'U') === -1)
+function hasVowel() {
+  var boxes = Array.from(document.getElementsByClassName("inner"));
+  var letters = boxes.map(function (box) {
+    return box.innerText
+  })
+  return /[AEIOU]/.test(letters.join(''))
 };
 
-var array1 = [];
-var array2 = [];
-var array3 = [];
-var array4 = [];
-var array5 = [];
+var selectedLetters = [];
+var completedWords = [];
+var scoreDisplay = [];
+var score = 0;
+
 /* Create a function to be used to add clicked letter to words element,
 and change the color of the box to orange */
 function buttonClick() {
-  var letter = event.target.innerHTML;
-  array1.push(letter);
-  array2 = array1.join('');
-  var displayedWord = document.getElementsByClassName("displayedWords")
-  for (var i = 0; i < displayedWord.length; i++) {
-    displayedWord[i].innerHTML = array2;
-  };
+  if (event.target.hasAttribute('disabled')) return;
+  var letter = event.target.innerText;
+  selectedLetters.push(letter);
+  var displayedWord = document.querySelector(".displayedWords")
+  displayedWord.innerHTML = selectedLetters.join('');
+  event.target.setAttribute('disabled', true);
   event.target.style.backgroundColor = '#FEA221';
-  event.target.disabled = true;
 };
-/* Add an event handler to utilize the buttonClick function; this function will
-remain commented out until it proves necessary */
-// var innerClass = document.getElementsByClassName("inner");
-// for (var i = 0; i < innerClass.length; i++) {
-//   innerClass[i].addEventListener("click", buttonClick());
-// };
 
 // Create a function that clears the words and all selected letters
 function clearWord() {
 // Loop through all of the boxes
-  var boxLetter = document.getElementsByClassName('inner');
-    for (var i = 0; i < boxLetter.length; i++) {
+  var boxes = document.getElementsByClassName('inner');
+  for (var i = 0; i < boxes.length; i++) {
 // Change the color of boxes to grey and reenable them
-      boxLetter[i].style.backgroundColor = '#999999';
-      boxLetter[i].disabled = false;
+    boxes[i].style.backgroundColor = '#999999';
+    boxes[i].removeAttribute('disabled');
   };
-  var displayedWord = document.getElementsByClassName("displayedWords");
-    for (var t = 0; t < displayedWord.length; t++) {
-      displayedWord[t].innerHTML = array3;
-  };
-  array1 = [];
-  array2 = [];
+  selectedLetters = [];
+  document.querySelector(".displayedWords").innerText = '';
 };
 
 /* Create a function that takes the length of a string and multiplies it by 9,
 then adds that value to points; then it will move the displayed word over to
 the side where a word column exists*/
 function submitWord() {
-  var displayedWord = document.getElementsByClassName("displayedWords");
-  var pointsDisplay = document.getElementsByClassName("points");
-  var wordsDisplay = document.getElementsByClassName("wordsList");
-  var playerScore = document.getElementsByClassName("number");
-  var pointsValue = array2.length * 9;
-  for (var i = 0; i < playerScore.length; i++) {
-    playerScore[i].innerHTML = array5
-  };
-  for (var i = 0; i < playerScore.length; i++) {
-    playerScore[i].innerHTML = array5 + pointsValue;
-  };
-  for (var z = 0; z < pointsDisplay.length; z++) {
-    pointsDisplay.innerHTML[z].unshift(pointsValue);
-  };
-  array4 = array2.split();
-  for (var w = 0; w < wordsDisplay.length; w++) {
-    wordsDisplay.innerHTML[w].unshift(array4);
-    wordsDisplay.innerHTML[w].join('');
-  };
+  var displayedWord = document.querySelector(".displayedWords");
+  var pointsDisplay = document.querySelector(".points");
+  var wordsDisplay = document.querySelector(".wordsList");
+  var playerScore = document.querySelector(".number");
+  score += selectedLetters.length * 9;
+  playerScore.innerText = score
+  scoreDisplay.unshift(selectedLetters.length * 9);
+  pointsDisplay.innerText = scoreDisplay;
+  completedWords.unshift(selectedLetters);
+  wordsDisplay.innerText = completedWords;
   clearWord();
 };
 
@@ -107,43 +85,30 @@ function submitWord() {
 re-randomizing the letters in the table */
 function resetWord() {
   clearWord();
-  var points = document.getElementsByClassName("points");
-  for (var i = 0; i < points.length; i++) {
-    points[i].innerHTML = [];
-  };
-  var list = document.getElementsByClassName("wordsList");
-  for (var q = 0; q < list.length; q++) {
-    list[q].innerHTML = [];
-  };
-  var number = document.getElementsByClassName("number");
-  for (var w = 0; w < number.length; w++) {
-    number[w].innerHTML = [];
-  };
+  var points = document.querySelector(".points");
+  points.innerText = '';
+  var list = document.querySelector(".wordsList");
+  list.innerText = '';
+  var number = document.querySelector(".number");
+  number.innerText = '';
   randomLetter();
-  vowelCheck();
 };
 
 // Create a function to unfade the submit button
-function unfadeSubmit() {
-  var submitButton = document.getElementsByClassName("submit");
-  submitButton.style.backgroundColor = '#C7E69A'
-};
+// function unfadeSubmit() {
+//   var submitButton = document.querySelector(".submit");
+//   submitButton.style.backgroundColor = '#C7E69A'
+// };
+// //
+// // Create a function to unfade clear button
+// function unfadeClear() {
+//   var clearButton = document.querySelector(".clear");
+//   clearButton.style.backgroundColor = '#F3AEB5'
+// };
+// // Add event listener for submit button to change color
+// document.querySelector(".submit");.addEventListener("DOMContentLoaded",
+// setTimeout(unfadeSubmit(), 5000));
 //
-// Create a function to unfade clear button
-function unfadeClear() {
-  var clearButton = document.getElementsByClassName("clear");
-  clearButton.style.backgroundColor = '#F3AEB5'
-};
-// Add event listener for submit button to change color
-var submitClass = document.getElementsByClassName("submit");
- for (var i = 0; i < submitClass.length; i++) {
-  submitClass[i].addEventListener("DOMContentLoaded", setTimeout(
-    unfadeSubmit(), 5000));
-};
-
-// Add event listener for clear button to change color
-var clearClass = document.getElementsByClassName("clear");
- for (var i = 0; i < clearClass.length; i++) {
-  clearClass[i].addEventListener("DOMContentLoaded", setTimeout(
-    unfadeClear(), 5000));
-};
+// // Add event listener for clear button to change color
+// document.querySelector(".clear");.addEventListener("DOMContentLoaded",
+// setTimeout(unfadeClear(), 5000));
